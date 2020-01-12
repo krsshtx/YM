7c518a463cc012ee78126154fc5e081b19729497
krsshtx <59250337+krsshtx@users.noreply.github.com>
2020-01-12 23:51:42 +0300

    Add files via upload
    ignore addreess - 21h - Rocket Knight Adventures

--- YM2612.cpp
+++ YM2612.cpp
@@ -24,17 +24,19 @@ void YM2612::Reset()
     GPIOB->regs->BSRR = (1U << 4) << (16 * 0); //_IC HIGH
     delayMicroseconds(5);
     GPIOB->regs->BSRR = (1U << 0) << (16 * 0); //_A1 HI
-    delayMicroseconds(5);
-    GPIOB->regs->BSRR = (1U << 4) << (16 * 0); //_IC HIGH
-    delayMicroseconds(5);
-    GPIOB->regs->BSRR = (1U << 4) << (16 * 1); //_IC LOW
-    delayMicroseconds(5);
-    GPIOB->regs->BSRR = (1U << 4) << (16 * 0); //_IC HIGH
-    delayMicroseconds(100);
+ ////   delayMicroseconds(5);
+ //   GPIOB->regs->BSRR = (1U << 4) << (16 * 0); //_IC HIGH
+ //   delayMicroseconds(5);
+//    GPIOB->regs->BSRR = (1U << 4) << (16 * 1); //_IC LOW
+ //   delayMicroseconds(5);
+ //   GPIOB->regs->BSRR = (1U << 4) << (16 * 0); //_IC HIGH
+//    delayMicroseconds(100);
 }
 
 void YM2612::Send(unsigned char addr, unsigned char data, bool setA1) //0x52 = A1 LOW, 0x53 = A1 HIGH
-{
+{ 
+  if (addr == 0x21) //Rocket Knight Adventures
+  return;
   switch(setA1)
   {
     case 0:
@@ -49,17 +51,17 @@ void YM2612::Send(unsigned char addr, unsigned char data, bool setA1) //0x52 = A
     GPIOB->regs->ODR &= ~(0x0808); //_CS LOW
         _bus->Write(addr);
     GPIOA->regs->ODR &= ~(0x1000); //_WR LOW
-    delayMicroseconds(1);
+    NOP;
     GPIOA->regs->ODR |= 0x1000;    //_WR HIGH
     GPIOB->regs->ODR |= 0x0808;    //_CS HIGH
     GPIOA->regs->ODR |= 0x0800;    //_A0 HIGH
     GPIOB->regs->ODR &= ~(0x0808); //_CS LOW
         _bus->Write(data);
     GPIOA->regs->ODR &= ~(0x1000); //_WR LOW
-    delayMicroseconds(1);
+    NOP;
  
     GPIOA->regs->ODR |= 0x1000;    //_WR HIGH
     GPIOB->regs->ODR |= 0x0808;    //_CS HIGH
     
-    delayMicroseconds(3);
+    delayMicroseconds(5);
 }
